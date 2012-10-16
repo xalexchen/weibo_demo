7150fe35767288b72cf0e58ded4ca8c92ccbfa3e
Alex <alex@miracle.com>
2012-10-16 11:06:36 +0800

    add tab menu layout

--- src/com/alex/weibo_demo/MainActivity.java
+++ src/com/alex/weibo_demo/MainActivity.java
@@ -2,23 +2,31 @@ package com.alex.weibo_demo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
 import android.support.v4.app.NavUtils;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity{
+	private static String TAG="Weibo";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+        setContentView(R.layout.activity_login);
+        ImageView mloginButton = (ImageView) findViewById(R.id.login_button);
+        mloginButton.setOnClickListener(new OnClickListener(){
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				setContentView(R.layout.activity_main);
+			}
+        	
+        });
+        Log.v(TAG,"oncreate ");
     }
-
-    
 }
